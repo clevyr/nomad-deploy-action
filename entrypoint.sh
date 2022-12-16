36ebe0d34557257aeb0425eb4e6d6c3adab3cbb1
aivrit <a.ivrit@gmail.com>
2022-12-16 19:25:28 +0000

    fix tmp string

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+JOB_NAME=sudo head -1 "$GITHUB_WORKSPACE/$NOMAD_JOB" | grep job | cut -d ' ' -f 2
 
 set -euxo pipefail
 
@@ -25,7 +26,6 @@ sudo apt-get update && sudo apt-get install nomad
 sudo apt-get install jq
 sed -i "s/\[\[\.version\]\]/$DOCKER_TAG/" "$GITHUB_WORKSPACE/$NOMAD_JOB"
 sudo head -1 "$GITHUB_WORKSPACE/$NOMAD_JOB" | grep job | cut -d ' ' -f 2
-JOB_NAME=sudo head -1 "$GITHUB_WORKSPACE/$NOMAD_JOB" | grep job | cut -d ' ' -f 2
 DONT_DEPLOY_CNC="${DONT_DEPLOY_CNC:-false}"
 if [ "$DONT_DEPLOY_CNC" = "true" ]; then
     CNC_LATEST_IMAGE=nomad job inspect $JOB_NAME | jq '.[].TaskGroups[].Tasks[].Config.image' | grep cnc | rev | cut -d ':' -f 1 | rev | cut -d '"' -f 1
